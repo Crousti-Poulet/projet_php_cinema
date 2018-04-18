37c86605af4e02099e275ad106707466c6fe5d2b
melinaWF3 <38463632+melinaWF3@users.noreply.github.com>
2018-04-18 09:54:03 +0200

    ajout colonnes table movies

--- sql/modifs_2.sql
+++ sql/modifs_2.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `movies` ADD `length` SMALLINT NOT NULL AFTER `title`;
 ALTER TABLE `movies` CHANGE `length` `length` SMALLINT(6) NOT NULL COMMENT 'en minutes';
+ALTER TABLE `movies` ADD `actors` VARCHAR(255) NULL AFTER `director`;
+ALTER TABLE `movies` ADD `genre` VARCHAR(255) NOT NULL AFTER `date_release`, ADD `country` VARCHAR(255) NOT NULL AFTER `genre`;
