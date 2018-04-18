e172a557dc28f0f9e1db2c0045479aa9135e6e4d
melinaWF3 <38463632+melinaWF3@users.noreply.github.com>
2018-04-18 10:35:13 +0200

    sql

--- sql/modifs_2.sql
+++ sql/modifs_2.sql
@@ -2,3 +2,4 @@ ALTER TABLE `movies` ADD `length` SMALLINT NOT NULL AFTER `title`;
 ALTER TABLE `movies` CHANGE `length` `length` SMALLINT(6) NOT NULL COMMENT 'en minutes';
 ALTER TABLE `movies` ADD `actors` VARCHAR(255) NULL AFTER `director`;
 ALTER TABLE `movies` ADD `genre` VARCHAR(255) NOT NULL AFTER `date_release`, ADD `country` VARCHAR(255) NOT NULL AFTER `genre`;
+ALTER TABLE `users` CHANGE `email` `email` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'unique, sert de login';
