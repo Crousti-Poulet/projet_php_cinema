d4ee21656aa0a110980337d1ff5ac0af6b0caa8c
mams <mam.raza@gmail.com>
2018-04-18 10:04:53 +0200

    ajout du header et footer
    inclusion du header et du footer dans index.php

---
+++ header.php
@@ -0,0 +1,35 @@
+
+
+<header class="container-fluid fixed-top">
+	
+	<div class="container   col-md-6 mt-sm-3 mt-md-4 ">
+		<!-- TITRE -->
+		<div class="row titre   ">
+			<h1 class="headerTitle col-md-12 text-center"> - Mon CinéMa - </h1>
+		</div>
+	
+	</div>
+
+	<!-- MENU -->
+	<div class=" container col-8  d-flex justify-content-center border-top border-bottom y">
+
+		<div class="col-sm-4 d-flex justify-content-center">
+			<a class="btn border-left border-right  col-sm-8 col-md-6" href="#" role="button"> - Acceuil -</a>
+		</div>	
+
+		<div class="col-sm-4 d-flex justify-content-center">
+			<a class="btn border-left border-right  col-sm-8 col-md-6" href="#" role="button"> - A l'affiche -</a>
+		</div>	
+		
+		<div class="col-sm-4 d-flex justify-content-center">
+			<a class="btn border-left border-right  col-sm-8 col-md-6" href="#" role="button"> - Contacts -</a>
+		</div>	
+
+	</div>
+
+
+</header>
+
+        
+
+	
