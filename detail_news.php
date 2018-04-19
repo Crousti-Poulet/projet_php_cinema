03164147e288aa25eb12cc4d2fb8e6a48a894b82
melinaWF3 <38463632+melinaWF3@users.noreply.github.com>
2018-04-19 14:59:48 +0200

    detail news avec jointure

--- detail_news.php
+++ detail_news.php
@@ -3,11 +3,14 @@
 
 	require 'includes/connect.php'; // pour se connecter à la BD
 
-
-		// nécessaire de récuperer l'ID mais inutile de l'afficher
-		$res = $bdd->prepare('SELECT id, title, user_id, content, img_path, date_created, date_updated FROM news ORDER BY date_created DESC'); 
-		$res->execute();
-		$news = $res->fetchAll();
+	if (isset($_GET['id']) && is_numeric($_GET['id'])){
+		$idNews =  $_GET['id'];
+	}
+	// récupération de la news et de son auteur
+	$res = $bdd->prepare('SELECT n.id, n.title, n.user_id, n.content, n.img_path, n.date_created, n.date_updated, u.firstname, u.lastname, u.email, u.role FROM news n LEFT JOIN users u on n.user_id = u.id WHERE n.id = :idNews'); 
+	$res->bindValue(':idNews', $idNews, PDO::PARAM_INT);
+	$res->execute();
+	$news = $res->fetch();
 
 
 ?>
@@ -15,7 +18,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Liste des actualités</title>
+	<title>Détail actualités</title>
 
 		<!-- Bootstrap -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -31,36 +34,33 @@
 
 	<main>
 
-		<h2>Liste des actualités</h2>
+		<h2>Actualité <?= $news['title']  ?></h2>
 
 
 		<div>
 			<table class="table table-striped table-hover" id="listeResas">
 				<thead class="thead-dark">
 					<tr>
-						<th>Détail</th>
 						<th>Titre</th>
-						<th>Auteur</th>
 						<th>Contenu</th>
 						<th>Image</th>
 						<th>Date création</th>
 						<th>Date modif.</th>
+						<th>Auteur</th>
+						<th>Mail auteur</th>
 					</tr>
 				</thead>
 				<tbody>
 					<?php
-						foreach($news as $new){
-							echo '<tr>';
-								echo '<td><a href="detail_news.php?id='.$new['id'].'"><i class="fas fa-search"></i></a></td>';
-								echo '<td><b>'.$new['title'].'</b></td>';
-								echo '<td>'.$new['user_id'].'</td>';
-								echo '<td>'.$new['content'].'</td>';
-								echo '<td><img src="'.$new['img_path'].'"></td>';
-								echo '<td>'.date('d/m/Y H:i', strtotime($new['date_created'])).'</td>';
-								echo '<td>'.date('d/m/Y H:i', strtotime($new['date_updated'])).'</td>';
-								
-							echo '</tr>';
-						}
+						echo '<tr>';
+							echo '<td><b>'.$news['title'].'</b></td>';
+							echo '<td>'.$news['content'].'</td>';
+							echo '<td><img src="'.$news['img_path'].'"></td>';
+							echo '<td>'.date('d/m/Y H:i', strtotime($news['date_created'])).'</td>';
+							echo '<td>'.date('d/m/Y H:i', strtotime($news['date_updated'])).'</td>';
+							echo '<td>'.$news['firstname'].' '.$news['lastname'].' ('.$news['role'].')</td>';
+							echo '<td>'.$news['email'].'</td>';
+						echo '</tr>';
 					?>
 				</tbody>
 			</table>
