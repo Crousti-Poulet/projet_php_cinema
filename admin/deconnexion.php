77998b28d385a8b334fb64ae8f0f8ca334736c6e
Ioulos <juliemenetrier2014@gmail.com>
2018-04-19 10:02:58 +0200

    mise ne place de la nav bar

--- admin/deconnexion.php
+++ admin/deconnexion.php
@@ -25,19 +25,7 @@ if(isset($_GET['logout']) && $_GET['logout'] == 'yes'){
 </head>
 <body>
 	
-	<header>
-		<nav class="container-fluid">
-			<div id="logo" class="menu-left">
-			</div>
-			<ul id="menu" class="menu-right">
-				<li class="menu-item"><a href="movie_list.php">Films</a></li>
-				<li class="menu-item"><a href="news_list.php">Actualités</a></li>
-				<li class="menu-item"><a href="users_list.php">Utilisateurs</a></li>
-				<li class="menu-item"><a href="add_user.php">Créer utilisateur</a></li>
-				<li class="menu-item"><a href="deconnexion.php">Deconnexion</a></li>
-			</ul>
-		</nav>
-	</header>
+	<?php include 'navbar.php' ?>
 
 	<main>
 
@@ -68,6 +56,14 @@ if(isset($_GET['logout']) && $_GET['logout'] == 'yes'){
 			
 		</div>
 	</main>
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<script  src="http://code.jquery.com/jquery-3.3.1.min.js"
+	integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+ 	crossorigin="anonymous"></script>
+ 	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
 
 </body>
---
+++ admin/navbar.php
@@ -0,0 +1,35 @@
+<header>
+
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	  <a class="navbar-brand" href="#">Navbar</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		
+			<ul class="navbar-nav mr-auto">
+				
+				<li class="nav-item">
+					<a class="nav-link" href="movie_list.php">Films</a>
+				</li>
+
+				<li class="nav-item">
+					<a class="nav-link" href="news_list.php">Actualités</a>
+				</li>
+
+				<li class="nav-item">
+					<a class="nav-link" href="users_list.php">Utilisateurs</a>
+				</li>
+
+				<li class="nav-item">
+					<a class="nav-link" href="add_user.php">Créer utilisateur</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="deconnexion.php">Deconnexion</a>
+				</li>
+
+			</ul>
+		</div>
+	</nav>
+</header>
