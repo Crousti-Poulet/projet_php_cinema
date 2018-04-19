672e710794eb68b13e722ce7e68f817b54d2c78e
Crousti-Poulet <el-ouni-mehdi@hotmail.fr>
2018-04-19 15:12:32 +0200

    update_opening_times completed
    Page update_opening_times enfin opérationnelle

--- admin/update_opening_times.php
+++ admin/update_opening_times.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 	session_start();
 
@@ -8,11 +8,11 @@
 	//Afin de pouvoir utiliser l'élément v de Respect/Validation
 	use Respect\Validation\Validator as v;
 
-	require '../includes/connect.php'; // On inclut le fichier "connect" servant à se connecter à la base de données.
+	require '../includes/connect.php'; // pour se connecter à la BD
 
 	if (!isset($_SESSION['user']) || empty($_SESSION['user'])){ // utilisateur non connecté
 
-		header('Location: connexion.php');
+		header('Location: ../connexion.php');
 		die();
 	}
 	// vérifier que l'utilisateur a le droit d'accéder à cette page : Admin = OK, Editeur = KO
@@ -22,53 +22,74 @@
 	}
 	else // utilisateur déjà connecté
 	{
-		
-		$errors = [];
-		$post = [];
 
-		// Si le formulaire a été soumis, la superglobale $_POST n'est pas vide
-		if(!empty($_POST)){ 
-			
-			if(empty($_POST['day'])) {
-				$errors[] = 'Veuillez choisir un jour.';
-			}
+		if (isset($_GET['day']) && !is_numeric($_GET['day'])){
 
-			if(!is_numeric($_POST['opening_hour']) || !is_numeric($_POST['opening_minute'])) {
-				$errors[] = 'Veuillez choisir une heure d\'ouverture valide.';
-			}
+			//récuperer les informations de la table
+			$res = $bdd->prepare('SELECT * FROM opening_times WHERE day = :day'); 
+			$res->bindValue(':day', $_GET['day']);
+			$res->execute();
+			$opening_day = $res->fetchAll(); 
 
-			if(!is_numeric($_POST['closing_hour']) || !is_numeric($_POST['closing_minute'])) {
-				$errors[] = 'Veuillez choisir une heure de fermeture valide.';
-			}
+			var_dump($opening_day[0]);
+
+
+			if (isset($opening_day) && !empty($opening_day)){
+
+				$errors = [];
+				$post = [];
+
+				// Si le formulaire a été soumis, la superglobale $_POST n'est pas vide
+				if(!empty($_POST)){ 
+
+					if(empty($_GET['day']) || $_GET['day'] !== $opening_day[0]['day']) {
+						$errors[] = 'Veuillez choisir un jour défini dans la base de donné.';
+					}
+
+					if(!is_numeric($_POST['opening_hour']) || !is_numeric($_POST['opening_minute'])) {
+						$errors[] = 'Veuillez choisir une heure d\'ouverture valide.';
+					}
+
+					if(!is_numeric($_POST['closing_hour']) || !is_numeric($_POST['closing_minute'])) {
+						$errors[] = 'Veuillez choisir une heure de fermeture valide.';
+					}
 				
-			if(count($errors) === 0){
+					if(count($errors) === 0){
 
-				$formValid = true;
+						$formValid = true;
 
-				//Variable permettant de reconstituer l'horaire sous forme d'une chaine de caractère.
-				$times = $_POST['opening_hour'].' h '.$_POST['opening_minute'].' ~ '.$_POST['closing_hour'].' h '.$_POST['closing_minute'];
+						//Variable permettant de reconstituer l'horaire sous forme d'une chaine de caractère.
+						$times = $_POST['opening_hour'].' h '.$_POST['opening_minute'].' ~ '.$_POST['closing_hour'].' h '.$_POST['closing_minute'];
 
-				$sth = $bdd->prepare('INSERT INTO opening_times (day, times) VALUES(:day, :times)');
+						$sth = $bdd->prepare('UPDATE opening_times SET times = :times WHERE day = :day');
 
-				$sth->bindValue(':day', $_POST['day']);
-				$sth->bindValue(':times', $times);
+						$sth->bindValue(':day', $_GET['day']);
+						$sth->bindValue(':times', $times);
+						var_dump($_GET['day']);
 
-				//Varibale permettant de vérifier si un jour a déjà été défini dans la table.
-				$success = $sth->execute();
+						//Varibale permettant de vérifier si un jour a déjà été défini dans la table.
+						$success = $sth->execute();
 
-				}  
-				else {
-				$formValid = false;
-			}
-		}
+						header('Location: opening_times_list.php');
+						die();
+
+						}  
+						else {
+						$formValid = false;
+					}
+					
+				} // fin du if(!empty($_POST))
+
+			} // fin du if (isset($movie) && !empty($movie))
+
+		} // fin du if (isset($_GET['id']) && is_numeric($_GET['id']))
 
 	} // fin du else du if (!isset($_SESSION['user']) || empty($_SESSION['user']))
 
- ?>
-
+?>
 <!DOCTYPE html>
 <html>
-	<head>
+<head>
 		<meta charset="utf-8"/>
 		<!-- Bootstrap start --> 
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -77,48 +98,43 @@
 		<!-- fonts start -->
 		<link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
 		<!-- fonts end -->
-		<title>Ajouter un horaire</title>
+		<title>Modifier une fiche de film</title>
 	</head>
 	<body>
-  
+
 	<?php include 'navbar.php' ?>
-			
-		<main class="container" id="main_bloc">
-			
-			<h2>Ajouter un horaire</h2>
-				
-			<!-- Affichage des messages d'erreur ou de confirmation -->
+
+	<main class="container">
+
+		<h2>Modification des horaires d'ouverture</h2>
+
+
+		<?php 
+			// afficher le formulaire seulement si le jour est trouvé
+			if (isset($_GET['day']) && !is_numeric($_GET['day'])):
+		 ?>
+
+			<!-- affichage du message de confirmation ou des erreurs -->
 			<?php if(isset($formValid) && $formValid == true && $success):?>
 
-			<p style="color:green;">Le nouvel horaire a bien été enregistré</p>
-			
-			<!-- Affichage d'un message d'erreur si le formulaire est valide mais que le jour est déjà présent dans la table -->
+			<p style="color:green;">Fiche de film modifiée avec succès !</p>
+		
 			<?php elseif(isset($formValid) && $formValid == true && !$success):?>
-			
-			<p style="color:red;">Horaire du jour déjà défini (pour toutes modifications, veuillez vous rendre sur la page de modification des horaires d'ouverture.</p>
-
+		
+			<p style="color:red;">Erreur, fiche non modifiée</p>
+		
 			<?php elseif(isset($formValid) && $formValid == false):?>
-			
-			<p style="color:red;"><?=implode('<br>', $errors);?></p>
+		
+			<p style="color:red;">
+				<?=implode('<br>', $errors);?>
+			</p>
 			<?php endif;?>
+
 
 			<div>
 				<form class="mx-auto" method="POST" id="formNewMovie" enctype="multipart/form-data">
-							
-					<div class="form-group">
-						<label for="day">Jour :</label>
-						<select class="form-control col-3" name="day" id="day">
-							<option <?php if (isset($_POST['day']) && $_POST['day']=="Lundi") echo 'selected'; ?> value="Lundi">Lundi</option>
-							<option <?php if (isset($_POST['day']) && $_POST['day']=="Mardi") echo 'selected'; ?> value="Mardi">Mardi</option>
-							<option <?php if (isset($_POST['day']) && $_POST['day']=="Mercredi") echo 'selected'; ?> value="Mercredi">Mercredi</option>
-							<option <?php if (isset($_POST['day']) && $_POST['day']=="Jeudi") echo 'selected'; ?> value="Jeudi">Jeudi</option>
-							<option <?php if (isset($_POST['day']) && $_POST['day']=="Vendredi") echo 'selected'; ?> value="Vendredi">Vendredi</option>
-							<option <?php if (isset($_POST['day']) && $_POST['day']=="Samedi") echo 'selected'; ?> value="Samedi">Samedi</option>
-							<option <?php if (isset($_POST['day']) && $_POST['day']=="Dimanche") echo 'selected'; ?> value="Dimanche">Dimanche</option>
-							<option <?php if (isset($_POST['day']) && $_POST['day']=="Férié") echo 'selected'; ?> value="Férié">Férié</option>
-						</select>
-					</div>
-
+					
+					<!-- Horaire d'ouverture -->
 					<div class="form-group">
 						<label for="opening_hour">Horaire d'ouverture : </label>					
 						<select class="form-control form-inline col-3" name="opening_hour" id="opening_hour">
@@ -139,7 +155,8 @@
 						<?php endfor; ?>							
 						</select>					
 					</div>
-
+					
+					<!-- Horaire de fermeture -->
 					<div class="form-group">
 						<label for="closing_hour">Horaire de fermeture : </label>			
 						<select class="form-control form-inline col-3" name="closing_hour" id="closing_hour">
@@ -165,7 +182,12 @@
 
 				</form>
 			</div>
-		</main>
+
+			<?php 
+				else: echo '<p style="color:red;">Les horaires de ce jour n\'ont pas encore été définis.</p>';
+			?>
+			<?php endif ?>
+	</main>
 
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
@@ -174,5 +196,6 @@
 	integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
  	crossorigin="anonymous"></script>
  	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-	</body>
+
+</body>
 </html>
